6d2490a0edad13576b41de1d5f568cf1ba59e309
Seroga Humenchuk <niva1999niva@gmail.com>
2018-10-01 23:03:43 +0300

    init

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,10 @@
-import {combineReducers} from '../../../../../../../../../AppData/Local/Microsoft/TypeScript/2.9/node_modules/redux';
-import value from './inputReducer';
-import weather from './weatherReducer';
+    import {combineReducers} from '../../../../../../../../../AppData/Local/Microsoft/TypeScript/2.9/node_modules/redux';
+    import value from './inputReducer';
+    import weather from './weatherReducer';
 
-const rootReducer = combineReducers({
-    value,
-    weather,
-})
+    const rootReducer = combineReducers({
+        value,
+        weather,
+    })
 
-export default rootReducer;
+    export default rootReducer;
