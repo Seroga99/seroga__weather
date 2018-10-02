d5dc7c5e492ec6c5219cdef5c9d31662e7a0647d
Seroga Humenchuk <niva1999niva@gmail.com>
2018-10-02 14:31:12 +0300

    ch

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,6 +1,6 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './reducers/index';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
 
 const DevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();  
 
